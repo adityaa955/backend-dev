b9c3e90da1b4a958b86de1a2217f18818ac58379
adityaa955 <adityanarayanshukla12345@gmail.com>
2026-05-05 15:48:46 +0530

    Delete e-commerce_project-Backend directory

--- e-commerce_project-Backend/backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUsers, registerUser } = require('../controllers/userController');
-
-router.route('/').get(getUsers).post(registerUser);
-
-module.exports = router;
